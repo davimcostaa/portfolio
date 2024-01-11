a1d8c947bf0f33b43cd74290e87a00c4c3464e97
Davi Costa <davim.costa@outlook.com>
2024-01-11 20:49:11 -0300

    Pastas e 'arquivos' funcionando.

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -4,16 +4,15 @@ import Menu from '@/src/components/Menu'
 import MenuMobile from '@/src/components/MenuMobile';
 import React, { useState } from 'react'
 import styled, { css } from 'styled-components';
-// import { Menu as DropDown} from 'antd'
 
 interface FolterContactProps {
   isOpen: boolean;
 }
 
 const Body = styled.div`
+    width: 100vw;
     padding-top: 56.191px;
     display: flex;
-    flex-direction: row;
     height: 100vh;
 `
 
@@ -46,21 +45,18 @@ const FileName = styled.div`
     cursor: pointer;
     height: 40.6px;
     width: 218.48px;
-    border-bottom: 0.5px solid #1E2D3D;
     border-right: 0.5px solid #1E2D3D;
     display: flex;
     align-items: center;
-    justify-content: space-around;
+    justify-content: space-between;
+    padding: 12px;
     gap: 7px;
-    margin-bottom: 16px;
     color: #607B96;
 `
-
 
 const Folder = styled.div`
     display: flex;
     flex-direction: column;
-    align-items: center;
     padding-left: 23px;
     gap: 13px;
     cursor: pointer;
@@ -71,7 +67,13 @@ const NomePasta = styled.div`
     display: flex;
     align-items: center;
     gap: 8.92px;
+`
 
+const SubPasta = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 8.92px;
+    padding-left: 40px;
 `
 
 const PrimeiraParte = styled.div`
@@ -143,6 +145,90 @@ const ContactText = styled.span`
 const CodeSection = styled.section`
       width: 674px;
       border-right: 0.5px solid #1E2D3D;
+
+      @media screen and (max-width: 1400px) {
+        width: 610px;
+      }
+
+`
+
+const Code = styled.section`
+    display: flex;
+    border-top: 0.5px solid #1E2D3D;
+    @media screen and (max-width: 1350px) {
+      scrollbar-width: thin;   
+      max-height: 470px;
+      overflow-y: auto;
+      &::-webkit-scrollbar {
+        width: 10px;
+      }
+    
+      &::-webkit-scrollbar-thumb {
+        background-color: #607B96;
+      }
+    
+      &::-webkit-scrollbar-track {
+        background-color: #011627;
+      }
+    }
+`
+
+const Numbers = styled.div`
+      width: 22px;
+      margin-top: 17.8px;
+      display: flex;
+      flex-direction: column;
+      direction: rtl;
+      margin-left: 38px;
+      font-size: 17px;
+      color: #607B96;
+      align-items: flex-start;
+      gap: 2px;
+
+      @media screen and (max-width: 1400px) {
+        margin-left: 28px;
+      }
+`
+
+const Text = styled.div`
+      width: 500px;
+      margin-top: 17.8px;
+      margin-left: 40px;
+      color: #607B96;
+      line-height: 150%; 
+
+      > p {
+        padding-left: 10px;
+      }
+
+      @media screen and (max-width: 1400px) {
+        margin-left: 35px;
+        > p {
+          padding-left: 10px;
+        }
+      }
+    
+`
+
+const TechnologiesSection = styled.section`
+    margin-top: 40.6px;
+    border-top: 0.5px solid #1E2D3D;
+    width: 745px;
+
+`
+
+const Post = styled.div`  
+`
+
+const User = styled.div`
+
+`
+
+const Profile = styled.img`
+    width: 36px;
+    height: 36px;
+    border-radius: 36px;
+
 `
 
 const AboutMe = () => {
@@ -152,7 +238,87 @@ const AboutMe = () => {
   const [interestsIsOpen, setInterestsIsOpen] = useState(false);
   const [educationIsOpen, setEducationIsOpen] = useState(false);
   const [contactIsOpen, setContactIsOpen] = useState(false);
+  const [numberOfLines, setNumberOfLines] = useState<number[]>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+
+  const aboutMe = `
+      * About me
+      * A 20-year-old professional with a technical 
+      * background in IT and a degree in
+      * Systems Analysis and Development, blending skills 
+      * in React, Next.js, Python,
+      * React Native, Express, and AdonisJS. Experienced 
+      * in IT support, advanced
+      * proficiency in English, and expertise in 
+      * automation and data processing with Python, as 
+      * well as creating applications with React.
+  `;
+
+  const experience = `
+      ** IT Support Intern
+      * IESB University Center 
+      * 
+      * March 2022 - June 2023.
+      
+          ---- Computer formatting;
+          ---- Administrative routines;
+          ---- Technical support campus staff.
+      * 
+      *   
+      *   
+      ** IT Intern
+      * National Indigenous People Foundation
+      * 
+      * July 2022 – December 2023.
+          ---- Python script coding for data processing.
+          ---- Python script coding for automation.
+          ---- Development of solutions using JavaScript.
+      * 
+      * 
+      *          
+      ** IT Intern
+      * National Indigenous People Foundation
+      *          
+      * July 2022 – December 2023.
+          ---- Python script coding for data processing.
+          ---- Python script coding for automation.
+          ---- Development of solutions using JavaScript.
+     *          
+
+  `;
+
+  const university = `
+      * University
+      *
+      * Graduated in Superior in Analysis and 
+      * Development of Systems by the 
+      * Institute of Superior Education of Brasilia.
+      * 
+      * Finished in December 2023.
+`;
+
   
+  const [openText, setOpenText] = useState(aboutMe);
+  const [fileName, setFileName] = useState('about-me');
+
+  const handleClick = (text: string, fileName: string) => {
+    setOpenText(text);
+    setFileName(fileName);
+    contarLinhas(text);
+  };
+
+  function contarLinhas(str: string) {
+    const linhas = str.split('\n');
+    const numeroDeLinhas = linhas.length;
+    const linesArray = []
+  
+    for (let index = 1; index <= numeroDeLinhas; index++) {
+      linesArray.push(index)
+    }
+
+    setNumberOfLines(linesArray)
+  }
+
+
   return (
     <>
       <Menu setMenuIsVisible={setMenuIsVisible} />
@@ -167,39 +333,35 @@ const AboutMe = () => {
                 personal-info
             </FolderName>
 
-            {/* <WhiteDropdown
-                style={{backgroundColor: '#011627', color: '#fff'}}
-                mode="inline"
-                items={[
-                  {label: "education", key: "education" ,children: [
-                    {label: "university", key: "university"}
-                  ]}
-                ]}
-              /> */}
-
               <div>
               <Folder onClick={() => setBioIsOpen(!bioIsOpen)}>
-                <PrimeiraParte>
-                {
-                  bioIsOpen ? <img src={`/arrow-open.png`} /> : <img src={`/arrow-right.png`} />
-                }
+                  <PrimeiraParte>
+                    {bioIsOpen ? <img src={`/arrow-open.png`} /> : <img src={`/arrow-right.png`} />}
 
-                  <NomePasta>
-                    <img src='/pink-icon.png' />
-                    bio
-                  </NomePasta>
-                </PrimeiraParte>
+                    <NomePasta>
+                      <img src='/pink-icon.png' />
+                      bio
+                    </NomePasta>
+                  </PrimeiraParte>
 
-                 {bioIsOpen ? (
-                    <div>
-                            <NomePasta >
-                            <img src='/file.png' />
-                            about-me
-                            </NomePasta>
+                  {bioIsOpen ? (
+                    <div onClick={(e) => { e.stopPropagation();  handleClick(aboutMe, 'about-me')}}>
+                        <SubPasta>
+                          <img src='/file.png' />
+                          about-me
+                        </SubPasta>
                     </div>
                   ) : null}
-                  
-              </Folder>
+
+                  {bioIsOpen ? (
+                    <div onClick={(e) => { e.stopPropagation();  handleClick(experience, 'experience')}}>
+                        <SubPasta>
+                          <img src='/file.png' />
+                          experience
+                        </SubPasta>
+                    </div>
+                  ) : null}
+                </Folder>
 
                   <Folder onClick={() => setInterestsIsOpen(!interestsIsOpen)}> 
 
@@ -224,17 +386,19 @@ const AboutMe = () => {
                       educationIsOpen ? <img src={`/arrow-open.png`} /> : <img src={`/arrow-right.png`} />
                       }
 
-                      <NomePasta>
+                      <NomePasta >
                         <img src='/blue-icon.png' />
                         education
                       </NomePasta>
                       </PrimeiraParte>
                     
                     {educationIsOpen ? (
-                        <NomePasta >
-                        <img src='/file.png' />
-                        university
-                        </NomePasta>
+                      <div onClick={(e) => { e.stopPropagation();  handleClick(university, 'university')}}>
+                        <SubPasta>
+                          <img src='/file.png' />
+                          university
+                        </SubPasta>
+                      </div>
                     ) : null}
 
                       
@@ -263,13 +427,42 @@ const AboutMe = () => {
         <CodeSection>
           
             <FileName>
-                personal-info
+                {fileName}
                 <img src='/close-icon.png' />
             </FileName>
 
+            <Code>
+                <Numbers>
+                  {numberOfLines.map((line) => (
+                    <span>{line}</span>
+                  ))}
+                </Numbers>
 
-
+                <Text>
+                /**
+                 {openText.split('\n').map((line, index) => (
+                  <p key={index}>{line}</p>
+                  ))}
+                */
+                </Text>
+            </Code>
+            
         </CodeSection>
+
+
+        <TechnologiesSection>
+            <Post>
+                <User>
+                    <div>
+                      <Profile src='/profile.jpg' />
+                    </div>
+
+                    <div>
+
+                    </div>
+               </User>
+            </Post>
+        </TechnologiesSection>
 
       </Body>
 
